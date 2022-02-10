ece61715c8197a94ad3c0caea6d6480557c3a4e3
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-02-11 00:46:36 +0530

    Added Consult Front Page

--- src/components/Pages/Consult.js
+++ src/components/Pages/Consult.js
@@ -1,14 +1,36 @@
 import React from "react";
 
-import './Consult.css';
+import "./Consult.css";
 
 function Consult() {
-    return(
-        <>
-          <h1>Consult</h1>
-        </>
-
-    )
+  return (
+    <div className="consult">
+      <div className="textSection">
+        <p className="heading">CONSULT</p>
+        <h1 className="head">Simple And Fast</h1>
+        <p className="headSub">Follow the below steps to get started</p>
+        <div className="processCard">
+          <p className="cardText"> Write about process here</p>
+        </div>
+        <div className="processCard">
+          <p className="cardText"> Write about process here</p>
+        </div>
+        <div className="processCard">
+          <p className="cardText"> Write about process here</p>
+        </div>
+        <div className="processCard">
+          <p className="cardText"> Write about process here</p>
+        </div>
+      </div>
+      <div className="imageSection">
+        <img
+          className="consultImage"
+          src="https://cdni.iconscout.com/illustration/premium/thumb/online-doctor-4538862-3783538.png"
+          alt="Online Doctor Consultation"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Consult;
