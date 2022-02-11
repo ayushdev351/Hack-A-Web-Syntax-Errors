ec9f9a873721cc5abc5f589df0228d530dae7b34
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-02-11 12:40:25 +0530

    Add Authentication Styles

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,57 +1,113 @@
-import React, { useState } from "react";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "firebase/auth";
-
-import { auth } from "../firebase/firebase-config";
+import React, { useRef } from "react";
+import styled from "styled-components";
+import { useUserContext } from "./UserContext";
+// import './index.css'
 
 const Signin = () => {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const emailRef = useRef();
+  const psdRef = useRef();
+  const { signInUser, forgotPassword } = useUserContext();
 
-  const [user, setUser] = useState({});
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
-
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = psdRef.current.value;
+    if (email && password) signInUser(email, password);
   };
 
-  const logout = async () => {
-    await signOut(auth);
+  const forgotPasswordHandler = () => {
+    const email = emailRef.current.value;
+    if (email)
+      forgotPassword(email).then(() => {
+        emailRef.current.value = "";
+      });
+    alert("Password Reset Instructions sent to your email");
   };
 
   return (
     <>
-      <p className="WebName">Welcome</p>
+      <WebName>Welcome To DoCall</WebName>
       <div className="form">
-        <div className="Form">
+        <Form>
           <h2> Login </h2>
-          <form onSubmit={login}>
-            <input placeholder="Email" type="email" />
+          <form onSubmit={onSubmit}>
+            <input placeholder="Email" type="email" ref={emailRef} />
             <br />
-            <input placeholder="Password" type="password" />
+            <input placeholder="Password" type="password" ref={psdRef} />
             <br />
             <button type="submit">Sign In</button>
-            {user ? loginEmail : "no"}
+            <p onClick={forgotPasswordHandler}>Forgot Password?</p>
           </form>
-        </div>
+        </Form>
       </div>
     </>
   );
 };
 
 export default Signin;
+
+const WebName = styled.div`
+  margin-top: 20px;
+  font-size: 40px;
+  font-family: cursive;
+`;
+
+const Form = styled.div`
+   margin-top: 7%;
+   padding: 20px;
+   margin-left: 30%;
+   margin-right: 30%;
+   
+   border-radius: 20px;
+   background-color: #6b0f1a;
+   background-image: linear-gradient(315deg, #6b0f1a 0%, #b91372 74%);
+   text-align : center; 
+
+   >h2{
+     color : white;
+     padding-bottom: 20px;
+     font-size : 35px;
+     font-family: cursive;  
+   }
+    
+    >form{
+      >input{
+        color: black;
+        height: 25px;
+        width: 50%;
+        margin: 10px;
+        border-color: white;
+        border-radius: 10px;
+        }
+
+        >button{
+          padding: 9px;
+          margin: 15px;
+          width: 30%;
+          border-radius: 5px;
+          background-color: white;
+          font-size: 15px;
+          font-weight: bold;
+          font-family: cursive;
+          color: black;
+          :hover{
+            cursor: pointer;
+            color: grey;
+          }
+        }
+
+        >p{
+          color: white;
+          padding-top: 10px;
+          padding-bottom: 13px;
+          font-weight: bold;
+          font-size: 20px;
+          font-family: cursive;
+          :hover{
+            cursor: pointer;
+            color: grey;
+          }
+        }
+      }
+    }
+`;
